9a162ddd4fefbe6b8394d6fa2f59cdea7221e004
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-07-16 12:30:32 +0530

    Fifth Day Commit 1

--- webapp/firstApp/views.py
+++ webapp/firstApp/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+import joblib
+import prediction_service
+import yaml
+import os
+import json
+import joblib
+import numpy as np
 
 # Create your views here.
 
+#cls=joblib.load('../prediction_service/model/model.joblib')
+
 def index(request):
     return render(request, "index.html")
+
+def result(request):
+    cls=joblib.load('../prediction_service/model/model.joblib')
+    lis=[]
+
+    lis.append(float(request.GET['age']))
+    lis.append(float(request.GET['sex']))
+    lis.append(float(request.GET['bmi']))
+    lis.append(float(request.GET['children']))
+    lis.append(float(request.GET['smoker']))
+    lis.append(float(request.GET['region']))
+
+    answer= cls.predict([lis]).tolist()[0]
+    return render(request, "index.html",{'answer':answer})
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -22,4 +22,5 @@ from firstApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('index/',views.result,name='result'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
