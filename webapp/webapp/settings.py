9369c6c87d3292ac634e83b9fb13fffd509e4955
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-07-17 20:47:18 +0530

    Day Seven

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c4k%t7olc2)6wsa!(26vcsmyone+r2!qpaac--4oygfd#6hg5l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://medical-insurance-xerxez.herokuapp.com']
+ALLOWED_HOSTS = ['medical-insurance-xerxez.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -132,4 +132,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT=os.path.join(BASE_DIR, 'static')
