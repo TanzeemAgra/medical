0111fce0ccdb54329fff7fb425fd5c66692fedad
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-07-17 21:20:06 +0530

    Day Seven 7

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
