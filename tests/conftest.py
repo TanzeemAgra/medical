0112661ce38943fba0e9a651c42fea3dade9431a
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-07-18 17:38:13 +0530

    MLFlow

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,15 @@ import yaml
 import os
 import json
 
-def test_generic():
-    a=2
-    b=2
-    assert a==b
+
+@pytest.fixture
+def config(config_path="params.yaml"):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+@pytest.fixture
+def schema_in(schema_path="schema_in.json"):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
