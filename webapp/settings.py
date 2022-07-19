69ccfcd46fd46f7e84ee06ec3fbe86db9c8f6a3c
Tanzeem Agra <tanzeem.agra@gmail.com>
2022-07-19 21:12:54 +0530

    MLFLOW 15

--- webapp/settings.py
+++ webapp/settings.py
@@ -13,8 +13,12 @@ import django_heroku
 
 
 import dj_database_url
+
+#################################################################
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#DATABASES['default'].update(db_from_env)
+
+##########################################################
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
